6acae484691b9b81556fb290ee8e0b4571ea3170
Shaqir Topko <topkoshaqir@gmail.com>
2024-06-04 00:28:56 +0200

    Shtimi i footer

---
+++ include/footer.php
@@ -0,0 +1,16 @@
+<footer>
+        <div class="footer-content">
+            <h3>Topko Dent</h3>
+            <p>"Embark on a journey beyond expectations, where your travel dreams come to life with personalized service and unrivaled expertise."</p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+        </div>
+        
+    </footer>
+</body>
+</html>
