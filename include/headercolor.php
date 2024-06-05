b1681afdf9a86d4af344f9f9be2f0ab97e75bcc3
Shaqir Topko <topkoshaqir@gmail.com>
2024-06-06 00:30:59 +0200

    Shtimi i nje header me color

---
+++ include/headercolor.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Destination Dreamers</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" type="image/x-icon" href="./images/Dr.Ardi.png">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>
+<header style="background-color: #B0C4DE">
+    <div class="container">
+        <div class="header-content">
+            <div class="logo">
+                <a href="index.php">
+                    <img src="./images/Dr.Ardi.png" style="width: 70px; height:50px;">
+                </a>
+            </div>
+            <div class="navbar">
+                <ul class="nav-items">
+                    <li><a  href='home.php'>Home</a></li>
+                    <li><a  href='aboutus.php'>About Us</a></li>
+                    <li><a  href='services.php'>Services</a></li>
+                    <li><a  href='before&after.php'>Before & After</a></li>
+                    <li><a  href='contact.php'>Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+
+<script src="js/script.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
