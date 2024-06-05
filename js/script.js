3c16dc980845704b1698f0876f6de3d2a9792d9d
Shaqir Topko <topkoshaqir@gmail.com>
2024-06-06 00:32:12 +0200

    Ndryshime ne js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,72 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var header = document.querySelector("header");
+  var header = document.querySelector("header");
+
+  window.addEventListener("scroll", function () {
+   
+    if (window.scrollY > header.offsetHeight) {
+     
+      header.classList.add("scrolled");
+    } else {
   
-    // Regjistrimi i një event listener për scroll
-    window.addEventListener("scroll", function () {
-      // Nëse scroll-i është më i madh se lartësia e header-it
-      if (window.scrollY > header.offsetHeight) {
-        // Shto klasën "scrolled"
-        header.classList.add("scrolled");
-      } else {
-        // Hiq klasën "scrolled"
-        header.classList.remove("scrolled");
-      }
-    });
+      header.classList.remove("scrolled");
+    }
   });
-  
-
-  document.addEventListener('DOMContentLoaded', function() {
-    const counters = document.querySelectorAll('.number');
-    const speed = 200; // Kohëzgjatja e animacionit për çdo numër
-
-    counters.forEach(counter => {
-        const animate = () => {
-            const value = +counter.getAttribute('data-number');
-            const data = +counter.innerText;
-
-            const time = value / speed;
-            if(data < value) {
-                counter.innerText = Math.ceil(data + time);
-                setTimeout(animate, 1);
-            } else {
-                counter.innerText = value;
-            }
-        };
-
-        let observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    animate();
-                    observer.unobserve(entry.target);
-                }
-            });
-        }, { threshold: 0.5 });
-
-        observer.observe(counter);
-    });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const counters = document.querySelectorAll('.number');
+  const speed = 200; 
+
+  counters.forEach(counter => {
+      const animate = () => {
+          const value = +counter.getAttribute('data-number');
+          const data = +counter.innerText;
+
+          const time = value / speed;
+          if(data < value) {
+              counter.innerText = Math.ceil(data + time);
+              setTimeout(animate, 1);
+          } else {
+              counter.innerText = value;
+          }
+      };
+
+      let observer = new IntersectionObserver((entries, observer) => {
+          entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                  animate();
+                  observer.unobserve(entry.target);
+              }
+          });
+      }, { threshold: 0.5 });
+
+      observer.observe(counter);
+  });
+});
+
+
+
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var images = document.querySelectorAll(".image-box img");
+var closeBtn = document.querySelector(".close");
+
+
+images.forEach(img => {
+img.addEventListener('click', function() {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+});
+});
+
+
+closeBtn.addEventListener('click', function() {
+modal.style.display = "none";
+});
+
+
+
 
