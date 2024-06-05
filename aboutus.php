57174c9a545a32dc0d2f5a80a242aa42f4444863
Shaqir Topko <topkoshaqir@gmail.com>
2024-06-06 00:32:52 +0200

    Shtohet pjesa e About Us

---
+++ aboutus.php
@@ -0,0 +1,174 @@
+<?php
+include "include/headercolor.php";
+?>
+
+
+<div class="wrapper rrethnesh">
+
+<div class="title">
+    <h4>Team Members</h4>
+</div>
+
+<div class="card_Container">
+
+    <div class="card" data-aos="zoom-in">
+
+        <div class="imbBx">
+            <img src="images/ardi.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <div class="contentBx">
+                <h3>Ardi TOPKO <br><span>Dentist</span></h3>
+            </div>
+            <ul class="sci">
+                <li style="--i: 1">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li style="--i: 3">
+                    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="card" data-aos="zoom-in">
+
+        <div class="imbBx">
+            <img src="images/vlera.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <div class="contentBx">
+                <h3>Vlera SALLA <br><span>Dentist for children</span></h3>
+            </div>
+            <ul class="sci">
+                <li style="--i: 1">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li style="--i: 3">
+                    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="card" data-aos="zoom-in">
+
+        <div class="imbBx">
+            <img src="images/doktor1.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <div class="contentBx">
+                <h3>Ilir GASHI <br><span>Orthodontist</span></h3>
+            </div>
+            <ul class="sci">
+                <li style="--i: 1">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li style="--i: 3">
+                    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="card" data-aos="zoom-in">
+
+        <div class="imbBx">
+            <img src="images/doktor3.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <div class="contentBx">
+                <h3>Erisa Dapko <br><span>Oral Surgeon</span></h3>
+            </div>
+            <ul class="sci">
+                <li style="--i: 1">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li style="--i: 3">
+                    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="card" data-aos="zoom-in">
+
+        <div class="imbBx">
+            <img src="images/doktor4.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <div class="contentBx">
+                <h3>DEA SKENDERI <br><span>Endodontist</span></h3>
+            </div>
+            <ul class="sci">
+                <li style="--i: 1">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li style="--i: 3">
+                    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="card" data-aos="zoom-in">
+
+        <div class="imbBx">
+            <img src="images/doktor2.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <div class="contentBx">
+                <h3>BENES FUSHA <br><span>General Dentist</span></h3>
+            </div>
+            <ul class="sci">
+                <li style="--i: 1">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li style="--i: 3">
+                    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    
+</div>
+
+</div>
+
+
+
+
+
+
+
+<script>
+  AOS.init();
+</script>
+
+
+
+
+
+<?php
+    include "include/footer.php";
+?>
+
+
+
+
+
+
+
