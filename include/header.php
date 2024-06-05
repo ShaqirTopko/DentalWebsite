163867644b8fbb017683def7908af2825d573d70
Shaqir Topko <topkoshaqir@gmail.com>
2024-06-06 00:30:18 +0200

    Disa ndryshime ne header

--- include/header.php
+++ include/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Destination Dreamers</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="icon" type="image/x-icon" href="./images/Dr.Ardi.png">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
@@ -26,9 +26,9 @@
             <div class="navbar">
                 <ul class="nav-items">
                     <li><a  href='home.php'>Home</a></li>
-                    <li><a  href='contact.php'>About Us</a></li>
-                    <li><a  href='contact.php'>Services</a></li>
-                    <li><a  href='contact.php'>Before & After</a></li>
+                    <li><a  href='aboutus.php'>About Us</a></li>
+                    <li><a  href='services.php'>Services</a></li>
+                    <li><a  href='before&after.php'>Before & After</a></li>
                     <li><a  href='contact.php'>Contact Us</a></li>
                 </ul>
             </div>
