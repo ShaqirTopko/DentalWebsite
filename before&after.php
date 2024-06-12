bcd2b27a838f06c9c5ea71424e7aa5d3a08c1cad
Shaqir Topko <topkoshaqir@gmail.com>
2024-06-12 13:58:42 +0200

    Before&after file

---
+++ before&after.php
@@ -0,0 +1,104 @@
+
+
+<?php
+include "include/header.php";
+?>
+
+<div class="slide-bar">
+        <div class="img" id="image-slider" loading="lazy">
+       <div class="diviboss"><h2 class="fjala">Before & After</h2></div>
+        </div>
+</div>
+
+<div class="containera" data-aos="zoom-in">
+        <h1>What We Do</h1>
+        <p>At Topko Dent, specializing in Implants & Periodontics, we provide advanced care dedicated to improving your oral health and transforming your smile. Dr. Ardi Topko and Dr. Vlera Salla, and our team of dedicated professionals, are eager to guide you through your treatment and ensure the best outcomes for your dental health.</p>
+       
+        <ul>
+            <li>Keep your teeth and gums healthy</li>
+            <li>Replace missing and damaged teeth</li>
+            <li>Treat and prevent periodontal disease</li>
+            <li>Restore oral health, function, and beauty</li>
+            <li>Ensure faster healing and recovery</li>
+            <li>Prevent future concerns for a long-lasting smile</li>
+        </ul>
+ 
+    </div>
+
+
+    <div class="appointment-section" data-aos="fade-up"
+     data-aos-duration="3000">
+        <h1>Request An Appointment To Get Started</h1>
+        <p>We've carefully planned every aspect of your visit. During your initial consultation, Dr. Ardi Topko will thoroughly assess your situation, discuss your goals, and review your available treatment options. In other words, he will listen to you and your needs.</p>
+        <a href="contact.php"><button class="button-19">Contact Us</button></a>
+    </div>
+
+
+    <div class = "texti" data-aos="fade-up"
+     data-aos-duration="500"> 
+
+      <h4>PATIENT</h4>
+
+      <h1>Transformation</h1>
+
+    </div>
+
+
+
+    <div class="container beforeafter">
+  <div class="image-box"><img src="images/njeri1.png" alt="Image 1" data-aos="zoom-out" loading="lazy"></div>
+  <div class="image-box"><img src="images/njeri2.jpg" alt="Image 2" data-aos="zoom-out" loading="lazy"></div>
+  <div class="image-box"><img src="images/njeri3.png" alt="Image 3" data-aos="zoom-out" loading="lazy"></div>
+  <div class="image-box"><img src="images/njeri4.jpg" alt="Image 4" data-aos="zoom-out" loading="lazy"></div>
+  <div class="image-box"><img src="images/njeri5.png" alt="Image 5" data-aos="zoom-out" loading="lazy"></div>
+  <div class="image-box"><img src="images/njeri6.png" alt="Image 6" data-aos="zoom-out" loading="lazy"></div>
+  <div class="image-box"><img src="images/njeri7.png" alt="Image 7" data-aos="zoom-out" loading="lazy"></div>
+  <div class="image-box"><img src="images/njeri8.jpg" alt="Image 8" data-aos="zoom-out" loading="lazy"></div>
+</div>
+
+<!-- Modal për imazhin e zmadhuar -->
+<div id="myModal" class="modal">
+  <span class="close">&times;</span>
+  <img class="modal-content" id="img01" loading="lazy">
+  <div id="caption"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<br>
+    <br>
+
+
+
+
+
+
+
+    <script src="js/script.js"></script>
+
+    <script>
+  AOS.init();
+</script>
+
+
+<?php
+include "pixijs1.php";
+?>
+
+
+
+
+    <br>
+    <br>
+
+<?php
+    include "include/footer.php";
+?>
